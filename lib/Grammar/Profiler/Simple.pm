595b4059a779426094ce8653b3830272842ba2ba
Jonathan Scott Duff <duff@pobox.com>
2011-12-03 12:51:03 -0600

    Updates

--- lib/Grammar/Profiler/Simple.pm
+++ lib/Grammar/Profiler/Simple.pm
@@ -4,10 +4,10 @@ my class ProfiledGrammarHOW is Metamodel::GrammarHOW is Mu {
 
     method find_method($obj, $name) {
         my $meth := callsame;
-        substr($name, 0, 1) eq '!' || $name eq any(<parse CREATE Bool defined MATCH WHAT gist>) ??
+        substr($name, 0, 1) eq '!' || $name eq any(<parse CREATE Bool defined MATCH WHAT perl>) ??
             $meth !!
             -> $c, |$args {
-                my $grammar = $obj.WHAT.gist;
+                my $grammar = $obj.WHAT.perl;
                 %timing{$grammar} //= {};                   # Vivify grammar hash
                 %timing{$grammar}{$meth.name} //= {};       # Vivify method hash
                 my %t := %timing{$grammar}{$meth.name};
@@ -24,8 +24,15 @@ my class ProfiledGrammarHOW is Metamodel::GrammarHOW is Mu {
     }
 }
 
-sub get-timing () is export { %timing }
-sub reset-timing () is export { %timing = () }
+proto sub get-timing is export { * }
+multi sub get-timing () { %timing }
+multi sub get-timing ($grammar) { %timing{$grammar} }
+multi sub get-timing ($grammar, $rule) { %timing{$grammar}{$rule} }
+
+proto sub reset-timing is export { * }
+multi sub reset-timing () { %timing = () }
+multi sub reset-timing ($grammar) { %timing{$grammar} = () }
+multi sub reset-timing ($grammar, $rule) { %timing{$grammar}{$rule} = () }
 
 my module EXPORTHOW { }
 EXPORTHOW.WHO.<grammar> = ProfiledGrammarHOW;
